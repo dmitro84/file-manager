b48d644d4502a1b466d79c601b10b9802820f632
dmitry.kuchukov <kudim84@gmail.com>
2022-12-17 21:19:02 +0300

    feat: implement Navigation & working directory

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import * as readline from 'node:readline/promises'
 import { getUsername } from './getUsername.js'
 import { __dirname } from './utils.js'
 import { homedir } from 'node:os'
+import { screenFolder } from './screenFolder.js'
+import { parse, isAbsolute } from 'path'
+import { stat } from 'fs'
+import { join } from 'node:path'
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 })
+
 const username = getUsername(process.argv)
+let currentDir = homedir()
 
 if (!username) {
-  console.log('Enter correct command.')
+  console.log('Error. Enter correct command.')
   rl.close()
 } else {
   console.log(`Welcome to the File Manager, ${username}!`)
-  console.log(`You are currently in ${homedir()}`)
+  console.log(`You are currently in ${currentDir}`)
 }
 
 rl.on('line', (line) => {
@@ -23,7 +29,51 @@ rl.on('line', (line) => {
     rl.close()
   }
 
-  console.log(`You are currently in ${__dirname}`)
+  if (line.toString().trim() === 'ls') {
+    screenFolder(currentDir).then(() =>
+      console.log(`You are currently in ${currentDir}`)
+    )
+  }
+
+  if (line.toString().trim() === 'up') {
+    currentDir = parse(currentDir).dir
+    console.log(`You are currently in ${currentDir}`)
+  }
+
+  if (line.toString().trim().indexOf('cd') === 0) {
+    if (line[2] === ' ' && line[3]) {
+      let inputPath = line.slice(3)
+      let futureDir = ''
+
+      if (isAbsolute(inputPath)) {
+        currentDir = inputPath
+        futureDir = join(inputPath)
+      } else {
+        if (inputPath.indexOf(':') === 1) {
+          futureDir = join(inputPath, '/')
+        } else {
+          futureDir = join(currentDir, inputPath)
+        }
+      }
+
+      stat(futureDir, function (err, stats) {
+        if (err) {
+          console.log('Path doesn`t exist.')
+        } else {
+          if (stats.isFile()) {
+            console.log('Path isn`t a folder')
+          } else {
+            currentDir = futureDir
+            console.log(`You are currently in ${currentDir}`)
+          }
+        }
+      })
+    } else {
+      console.log('Error. Enter correct command.')
+    }
+  }
+
+  //   console.log(`You are currently in ${currentDir}`)
 })
 
 rl.on('SIGINT', () => {
--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,7 @@ import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
 export const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export const byField = (field) => {
+  return (a, b) => (a[field] > b[field] ? 1 : -1)
+}
