392d7e2086ef06d16e71ae1d80931b8a8310e933
dmitry.kuchukov <kudim84@gmail.com>
2022-12-17 14:15:52 +0300

    feat: impement start app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-import { __dirname } from './utils.js';
+import * as readline from 'node:readline/promises'
+import { getUsername } from './getUsername.js'
+import { __dirname } from './utils.js'
+import { homedir } from 'node:os'
 
-console.log(__dirname);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+const username = getUsername(process.argv)
+
+if (!username) {
+  console.log('Enter correct command.')
+  rl.close()
+} else {
+  console.log(`Welcome to the File Manager, ${username}!`)
+  console.log(`You are currently in ${homedir()}`)
+}
+
+rl.on('line', (line) => {
+  if (line.toString().trim() === '.exit') {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`)
+    rl.close()
+  }
+
+  console.log(`You are currently in ${__dirname}`)
+})
+
+rl.on('SIGINT', () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`)
+
+  rl.close()
+})
